52925345ed741e8a16c57d984c5d0eb0fb377ee9
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2019-08-22 14:12:05 -0300

    validating number of CPF characters

--- src/Validate.php
+++ src/Validate.php
@@ -4,4 +4,38 @@ namespace CPF;
 
 class Validate{
     
+    private $cpf;
+
+    public function validate(string $cpf)
+    {
+        $this->setCpf($cpf);
+    }
+
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf($cpf)
+    {
+
+        if(!ctype_digit($cpf)){
+        
+            if(strlen($cpf) != 14)
+                throw new \Exception("Invalid CPF, must have 14.", 1);
+
+            $pattern = '/(.)(-)/';
+
+            if (!preg_match($pattern, $cpf)) 
+                throw new \Exception("CPF is invalid character", 1);
+                
+            
+        } else {
+            if(strlen($cpf) != 11)
+                throw new \Exception("Invalid CPF, must have 11.", 1);
+            
+        }
+        $this->cpf = $cpf;
+    }
+
 }
