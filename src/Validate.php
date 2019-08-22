2fbec71cbb0e16e097eb0011b864ec45adc6b4e3
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2019-08-22 17:10:02 -0300

    VerifiedNumber() and CheckDigit() function created

--- src/Validate.php
+++ src/Validate.php
@@ -5,10 +5,45 @@ namespace CPF;
 class Validate{
     
     private $cpf;
+    private $verifyingDigit;
 
     public function validate(string $cpf)
     {
         $this->setCpf($cpf);
+        return $this->validateVerifiedNumber();
+    }
+
+    public function validateVerifiedNumber()
+    {
+        $cpf = (int)substr($this->getCpf(), 0, 9);
+        $this->setVerifyingDigit(substr($this->getCpf(), 9, 2));
+        
+        $checkerNumber1 = $this->checkDigit($cpf, 10);
+        $checkerNumber2 =$this->checkDigit($cpf, 11,  $checkerNumber1);
+        return ($checkerNumber1.$checkerNumber2 == substr($this->getCpf(), 9, 2));
+    }
+
+    private function checkDigit($cpf, $sum, $firstTester = '')
+    {
+        $calc = 0;
+        $cpf .= $firstTester;
+        
+        for ($i=strlen((int)$cpf); $i > 0; $i--) { 
+            
+            $calc = substr(strrev($cpf), $i - 1, 1) * (1 + $i) + $calc;
+            
+        }   
+        
+        
+        $restofDivision = floor($calc % 11 );
+        
+        
+        if($restofDivision < 2){
+            $verifyingDigit = 0;
+        } else {
+            $verifyingDigit = $restofDivision - 11;
+        }
+        return abs($verifyingDigit);
     }
 
     public function getCpf()
@@ -26,16 +61,29 @@ class Validate{
 
             $pattern = '/(.)(-)/';
 
+            
             if (!preg_match($pattern, $cpf)) 
                 throw new \Exception("CPF is invalid character", 1);
-                
+
+            $cpf = str_replace('.', '', $cpf);
+            $cpf = str_replace('-', '', $cpf);
+            
             
         } else {
             if(strlen($cpf) != 11)
                 throw new \Exception("Invalid CPF, must have 11.", 1);
-            
         }
         $this->cpf = $cpf;
+    }
+
+    public function getVerifyingDigit()
+    {
+        return $this->verifyingDigit;
+    }
+
+    public function setVerifyingDigit($verifyingDigit)
+    {
+        $this->verifyingDigit = $verifyingDigit;
     }
 
 }
